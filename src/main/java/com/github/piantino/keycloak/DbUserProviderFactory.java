c72f873827542d3a569cd8f62fc54e91e1adb911
Andre Piantino <piantino@gmail.com>
2024-03-22 12:28:23 -0300

    Fixed log on import error

--- src/main/java/com/github/piantino/keycloak/DbUserProviderFactory.java
+++ src/main/java/com/github/piantino/keycloak/DbUserProviderFactory.java
@@ -141,14 +141,14 @@ public class DbUserProviderFactory implements UserStorageProviderFactory<DbUserP
     public static AgroalDataSourceMetrics getDataSourceMetrics(RealmModel realm) {
         UserStorageProviderModel model = getModel(realm);
         return getDataSource(model).getMetrics();
-	}
+    }
 
     public static UserStorageProviderModel getModel(RealmModel realm) {
-		return realm.getUserStorageProvidersStream()
-				.filter(fedProvider -> Objects.equals(fedProvider.getProviderId(), DbUserProviderFactory.PROVIDER_ID))
-				.findFirst()
-				.orElseThrow(() -> new DbUserProviderException(DbUserProviderFactory.PROVIDER_ID + " not configured"));
-	}
+        return realm.getUserStorageProvidersStream()
+                .filter(fedProvider -> Objects.equals(fedProvider.getProviderId(), DbUserProviderFactory.PROVIDER_ID))
+                .findFirst()
+                .orElseThrow(() -> new DbUserProviderException(DbUserProviderFactory.PROVIDER_ID + " not configured"));
+    }
 
     private SynchronizationResult importUsers(KeycloakSessionFactory sessionFactory, String realmId,
             UserStorageProviderModel model, String sql, Consumer<PreparedStatement> psConsumer) {
@@ -198,7 +198,7 @@ public class DbUserProviderFactory implements UserStorageProviderFactory<DbUserP
                                 }
                             } catch (Throwable e) {
                                 result.increaseFailed();
-                                LOGGER.errorv(e, "{0} - Error on import {1}",
+                                LOGGER.errorv(e, "{0} - Error on import {1}", realmId,
                                         data.get(DbUserProvider.Column.username.name()));
                             }
                         }
