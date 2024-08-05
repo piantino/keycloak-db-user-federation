d173c4134179e389af42268fd429ceeb09487114
Andre Piantino <piantino@gmail.com>
2024-08-05 17:26:00 -0300

    Improved log on import error

--- src/main/java/com/github/piantino/keycloak/DbUserProviderFactory.java
+++ src/main/java/com/github/piantino/keycloak/DbUserProviderFactory.java
@@ -199,8 +199,7 @@ public class DbUserProviderFactory implements UserStorageProviderFactory<DbUserP
                                 }
                             } catch (Throwable e) {
                                 result.increaseFailed();
-                                LOGGER.errorv(e, "{0} - Error on import {1}", realmId,
-                                        data.get(DbUserProvider.Column.username.name()));
+                                LOGGER.errorv(e, "{0} - Error on import {1}", realmId, username);
                             }
                         }
                     });
